e1b6c76fb01952a48deec3cf37b3db5a9c2c155f
Derek Sung <d.sung@ufl.edu>
2026-04-08 18:06:25 -0400

    fixed all bugs and logic errors

--- calculator.py
+++ calculator.py
@@ -11,9 +11,15 @@ calculator.py
 One function per operation, in order.
 """
 # First example
+def square_root(a):
+    if a<0:
+        raise ValueError("Square root of non-positive number")
+    return math.sqrt(a)
+def hypotenuse(a,b):
+    return math.hypot(a , b)
 def add(a, b): 
     return a + b
-def sub(a, b):
+def subtract(a, b):
     return a - b
 def mul(a, b):
     return a * b
@@ -21,18 +27,14 @@ def div(a, b):
     if a == 0:
         raise ZeroDivisionError("Divide by zero")
     return b / a
-def log(a,b):
-    if b <= 0:
+def logarithm(a,b):
+    if b <= 0 or a <= 0:
         raise ValueError("Logarithm of non-positive number")
-    return math.log(a, b)
+    if math.log(a) == 0:
+        raise ValueError("Undefined Logarithm")
+    return math.log(b)/math.log(a)
 def exp(a,b):
     return a ** b
-def square_root(a):
-    if a<0:
-        raise ValueError("Square root of non-positive number")
-    return math.sqrt(a)
-def hypotenuse(a,b):
-    return math.hypot(a , b)
 
 
 
--- test_calculator.py
+++ test_calculator.py
@@ -1,35 +1,35 @@
 #https://github.com/Dsung0107/lab11-DS-YJ.git
 #Partner 1: Derek Sung
 #Partner 2: Yoonho Ji
-
+import math
 import unittest
-from calculator import add, sub, mul, div, log, exp
+from calculator import add, subtract, mul, div, logarithm, exp, square_root, hypotenuse
 
 class TestCalculator(unittest.TestCase):
 
     def test_add(self): # 3 assertions
         self.assertEqual(add(2,3), 5)
-        self.assertEqual(add(1,1),0)
+        self.assertEqual(add(1,1),2)
         self.assertEqual(add(0,0),0)
         self.assertAlmostEqual(add(1.5,2.5),4)
 
 
     def test_subtract(self): # 3 assertions
-        self.assertEqual(sub(5,3),2)
-        self.assertEqual(sub(0,0),0)
-        self.assertEqual(sub(-2, -3), -5)
-        self.assertAlmostEqual(sub(3.5,1.5),2.0)
+        self.assertEqual(subtract(5,3),2)
+        self.assertEqual(subtract(0,0),0)
+        self.assertEqual(subtract(-2, -3), 1)
+        self.assertAlmostEqual(subtract(3.5,1.5),2.0)
 
     ######## Partner 1
     def test_multiply(self): # 3 assertions
         self.assertEqual(mul(2,2), 4)
         self.assertEqual(mul(1,500), 500)
-        self.assertequal(mul(0,0), 0)
+        self.assertEqual(mul(0,0), 0)
 
     def test_divide(self): # 3 assertions
         self.assertEqual(div(2,2), 1)
-        self.assertEqual(div(1,2), 0.5)
-        self.assertAlmostEqual(div(1,3), 0.33333333333)
+        self.assertEqual(div(2,1), 0.5)
+        self.assertAlmostEqual(div(3,1), 0.33333333333)
     ######## Partner 2
     def test_divide_by_zero(self): # 1 assertion
         with self.assertRaises(ZeroDivisionError):
@@ -37,18 +37,18 @@ class TestCalculator(unittest.TestCase):
 
 
     def test_logarithm(self): # 3 assertions
-        self.assertAlmostEqual(log(10,100),2.0)
-        self.assertAlmostEqual(log(2,8),3)
-        self.assertAlmostEqual(log(math.e,math.e),1)
+        self.assertAlmostEqual(logarithm(10,100),2.0)
+        self.assertAlmostEqual(logarithm(2,8),3)
+        self.assertAlmostEqual(logarithm(math.e,math.e),1)
 
 
     def test_log_invalid_base(self): # 1 assertion
         with self.assertRaises(ValueError):
-            log(1,10)
+            logarithm(1,10)
         with self.assertRaises(ValueError):
-            log(-2,10)
+            logarithm(-2,10)
         with self.assertRaises(ValueError):
-            log(2,-5)
+            logarithm(2,-5)
 
 
     # ##########################
@@ -56,7 +56,7 @@ class TestCalculator(unittest.TestCase):
     ######## Partner 1
     def test_log_invalid_argument(self):
         with self.assertRaises(ValueError):
-            log(2, -1)
+            logarithm(2, -1)
     def test_hypotenuse(self):
         self.assertEqual(hypotenuse(3,4), 5)
         self.assertAlmostEqual(hypotenuse(1,1), 1.4142136 )
