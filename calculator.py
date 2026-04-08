d4bedcef8abf6ab0b175be84cbd2decdb469c993
Derek Sung <d.sung@ufl.edu>
2026-04-08 17:07:28 -0400

    modified calculator p1

--- calculator.py
+++ calculator.py
@@ -1,3 +1,4 @@
+import math
 """
 calculator.py
 - Defines functions used to create a simple calculator
@@ -6,6 +7,22 @@ One function per operation, in order.
 """
 # First example
 def add(a, b): 
-    pass
+    return a + b
+def subtract(a, b):
+    return a - b
+def multiply(a, b):
+    return a * b
+def divide(a, b):
+    if a == 0:
+        raise ZeroDivisionError("Divide by zero")
+    return b / a
+def logarithm(a,b):
+    if b <= 0:
+        raise ValueError("Logarithm of non-positive number")
+    return math.log(a, b)
+def exponent(a,b):
+    return a ** b
+
+
 
 
